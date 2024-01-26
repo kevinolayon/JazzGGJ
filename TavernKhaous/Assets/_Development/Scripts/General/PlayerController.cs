638a9e62cd58abd5480d40b7184a324670b38be0
kevin-navarro <kevin.olayon@live.com>
2024-01-26 11:01:14 -0300

    Ragdoll improvements, new character, lightning and level tests

--- TavernKhaous/Assets/_Development/Scripts/General/PlayerController.cs
+++ TavernKhaous/Assets/_Development/Scripts/General/PlayerController.cs
@@ -1,24 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    CanvasManager canvas;
+    [Header("Settings")]
+    [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float rotationSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+
+    [Header("References")]
+    [SerializeField] Rigidbody rb;
+    [SerializeField] ConfigurableJoint cj;
+
+    float horizontal;
+    float vertical;
+
+    bool isRotating;
+    bool isMoving;
+
+    float initialSpeed;
+    Quaternion newRotation;
 
     private void Start()
     {
-        canvas = CanvasManager.Instance;
+        initialSpeed = movementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+        Rotate();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            canvas.ToggleImage();
+        Jump();
+    }
 
-            SceneManager.LoadScene(1);
+    void Movement()
+    {
+        vertical = Input.GetAxis("Vertical"); // Get input to move
+
+        //movementSpeed = isRotating ? movementSpeed / 2 : initialSpeed;
+
+        isMoving = vertical != 0; // Toggle bool if is moving or not
+
+        rb.AddForce(vertical * movementSpeed * rb.transform.forward); // Add force to movement the character
+    }
+
+    void Rotate()
+    {
+        horizontal = Input.GetAxis("Horizontal"); // Get input to rotate
+
+        isRotating = horizontal != 0;  // Toggle bool if is rotating or not
+
+        newRotation = Quaternion.Euler(0, -horizontal * rotationSpeed, 0); // Calculate the new rotation
+
+        cj.targetRotation *= newRotation; // Apply rotation to the joint
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
